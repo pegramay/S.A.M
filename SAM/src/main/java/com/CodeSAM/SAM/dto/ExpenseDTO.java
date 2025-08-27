076b0862958e1db9d901562f18ba19bd0b212ee7
pegramay <alexyoungsoopegram@gmail.com>
2025-08-26 23:01:02 -0400

    Initial Spring Project

---
+++ SAM/src/main/java/com/CodeSAM/SAM/dto/ExpenseDTO.java
@@ -0,0 +1,17 @@
+package com.CodeSAM.SAM.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class ExpenseDTO {
+
+    private Long id;
+
+    private String title;
+    private String description;
+    private String category;
+    private LocalDate date;
+    private Integer amount;
+}
